a8010d2d6931edb39b4f7d9ea980603d3d5a6b4c
Andy Waite <andy@andywaite.com>
2009-10-01 01:04:46 +0100

    First release

--- lib/dehumanize.rb
+++ lib/dehumanize.rb
@@ -1,1 +1,11 @@
 # Dehumanize
+module ActiveSupport::Inflector
+  def dehumanize(the_string)
+    the_string.gsub(' ', '_').downcase
+  end
+end
+class String
+  def dehumanize
+    ActiveSupport::Inflector.dehumanize(self)
+  end
+end
