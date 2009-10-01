0b475cdafcf06f7c6c75016301405eb2baa07542
Andy Waite <andy@andywaite.com>
2009-10-01 01:14:12 +0100

    Minor cleanup

--- lib/dehumanize.rb
+++ lib/dehumanize.rb
@@ -1,9 +1,9 @@
-# Dehumanize
 module ActiveSupport::Inflector
   def dehumanize(the_string)
     the_string.gsub(' ', '_').downcase
   end
 end
+
 class String
   def dehumanize
     ActiveSupport::Inflector.dehumanize(self)
